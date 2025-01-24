75de579db61bcc9c1a4d29e09985392fc3d4d212
Andre <apascal.pm@gmail.com>
2025-01-24 01:24:21 -0500

    mobile set but menu not working

---
+++ .history/assets/js/mobile_20250124005953.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Toggle navigation visibility for mobile
+    $('.nav-toggle').on('click', function () {
+        $('.navBar').toggleClass('show');
+    });
+});
