94ecee1ae13f6c66c9df7a8f77c43bae3e1ccd4f
Andre <apascal.pm@gmail.com>
2025-01-30 00:22:23 -0500

    format navbar and have it move to section

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,6 +1,12 @@
-$(document).ready(function () {
-    // Toggle navigation visibility for mobile
-    $('.nav-toggle').on('click', function () {
-        $('.navBar').toggleClass('show');
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the mobile menu and the toggle button
+    const mobileMenu = document.getElementById('mobile-menu');
+    const toggleButton = document.querySelector('.nav-toggle');
+
+    // Add a click event listener to the toggle button
+    toggleButton.addEventListener('click', function () {
+        // Toggle the 'show' class on the mobile menu
+        mobileMenu.classList.toggle('show');
+        
     });
 });
